86d05d9e658ad7cc84609057588f293f36b53197
Mathieu Hingant <mathieu@themojoes.eu>
2016-02-04 11:10:14 +0200

    Added functions to get/set some parameters. Cannot have both OF grabber and libuvc working at same time :(.

--- example/src/ofApp.cpp
+++ example/src/ofApp.cpp
@@ -2,70 +2,34 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
+    cam.setup(640, 480);
+    
     uvc.setup();
+    uvc.setAutoExposure(1); // manual
+    exposure = uvc.getExposureAbs();
 }
 
 //--------------------------------------------------------------
 void ofApp::update(){
-
+    cam.update();
 }
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    cam.draw(0, 0);
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::keyReleased(int key){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseMoved(int x, int y ){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseDragged(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mousePressed(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseReleased(int x, int y, int button){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseEntered(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::mouseExited(int x, int y){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::windowResized(int w, int h){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::gotMessage(ofMessage msg){
-
-}
-
-//--------------------------------------------------------------
-void ofApp::dragEvent(ofDragInfo dragInfo){ 
-
+    if(key == '+') {
+        exposure++;
+        uvc.setExposureAbs(exposure);
+    }
+    else if(key == '-') {
+        exposure--;
+        uvc.setExposureAbs(exposure);
+    }
+    else if(key == 'i') {
+        ofLogNotice("ofApp") << "Exposure Absolute = " << uvc.getExposureAbs();
+    }
 }
